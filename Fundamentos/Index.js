527cfd44a9a7fef389d0359c3b33ea5a4713ad87
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-29 11:06:46 -0600

    ejericicos de operaciones basicas

--- Fundamentos/Index.js
+++ Fundamentos/Index.js
@@ -7,6 +7,10 @@ const Resta = (num1, num2)=>{
 const Multi = (num1, num2)=>{
     return num1 * num2
 }
-console.log("suma "+Sumar(2,2))
-console.log("resta "+Resta(2323,12121))
-console.log("multiplicacion " +Multi(232323,23232323))
+const Division = (num1, num2)=>{
+    return num1 / num2
+}
+console.log(`El resultado de la suma es: ${Sumar(3434, 232)}`)
+console.log(`El resultado de la restaa es: ${Resta(2323, 543)}`)
+console.log(`El resultado de la multiplicacion es: ${Multi(1212, 23238723)}`)
+console.log(`El resultado de la division es: ${Division(12, 32)}`)
