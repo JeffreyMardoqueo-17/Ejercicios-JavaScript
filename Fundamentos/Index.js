a7917f7372aaed252b237f742addb9b04e44257f
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-29 10:57:34 -0600

    operaciones basicas

---
+++ Fundamentos/Index.js
@@ -0,0 +1,12 @@
+const Sumar = (num1, num2)=>{
+    return num1 + num2
+}
+const Resta = (num1, num2)=>{
+    return num1 - num2
+}
+const Multi = (num1, num2)=>{
+    return num1 * num2
+}
+console.log("suma "+Sumar(2,2))
+console.log("resta "+Resta(2323,12121))
+console.log("multiplicacion " +Multi(232323,23232323))
